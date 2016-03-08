3b71c0c712dc4af0e51d080d23a2d5dbbbf24877
Adriana <dri19tcc@gmail.com>
2016-03-08 14:00:55 -0800

    updated our TDD to pass in a word and see if the word is split.  Made our code return an array of characters.  Tests pass.

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,8 +1,16 @@
 class Scoring
-
-  
+  # SCORE_CHART = [
+  #   [a, e, i, o, u, l, n, r, s, t = 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  #   [d, g = 2, 2],
+  #   [b, c, m, p = 3, 3, 3, 3],
+  #   [f, h, v, w, y = 4, 4, 4, 4, 4],
+  #   [k = 5],
+  #   [j, x = 8, 8],
+  #   [q, z = 10, 10]
+  # ]
 
   def self.score(word)
-    word
+    arr = word.split(//)
+    return arr 
   end
 end
--- spec/scoring-spec.rb
+++ spec/scoring-spec.rb
@@ -11,15 +11,22 @@ describe Scoring do
       "Scoring#score(word).wont_be_nil"
     end
 
-    SCORE_CHART = {
-      a, e, i, o, u, l, n, r, s, t => 1,
-      d, g => 2,
-      b, c, m, p => 3,
-      f, h, v, w, y => 4,
-      k => 5,
-      j, x => 8,
-      q, z => 10
-    }
+    # SCORE_CHART = [
+    #   [a, e, i, o, u, l, n, r, s, t = 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    #   [d, g = 2, 2],
+    #   [b, c, m, p = 3, 3, 3, 3],
+    #   [f, h, v, w, y = 4, 4, 4, 4, 4],
+    #   [k = 5],
+    #   [j, x = 8, 8],
+    #   [q, z = 10, 10]
+    # ]
+
+    describe "Scoring#score(word)" do
+      it "Does it split the word into an array" do
+        assert Scoring.score("word") == ["w", "o", "r", "d"]
+        return
+      end
+    end
 
   end
 end
