2f912c66ee44468fb4b47c8e80086881c6ee2f16
Risha <rishallen@gmail.com>
2016-03-08 14:14:37 -0800

    added the score method that gets the score for each word.

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,3 +1,5 @@
+require_relative '../scrabble'
+
 class Scrabble::Scoring
 
   LETTERS = {
@@ -10,14 +12,32 @@ class Scrabble::Scoring
   %w(Q Z) => 10
 }
 
-def self.letter_value(letter)
-    LETTERS.each do |key, value|
-        if key.include? letter
-            return value
-        end
+  def self.letter_value(letter)
+      LETTERS.each do |key, value|
+          if key.include? letter
+              return value
+          end
+      end
+  end
+
+  def get_score(word_array)
+    word_score = 0
+    word_array.each do |letter|
+      temp_letter_val = letter_value(letter)
+      word_score += temp_letter_val
     end
+  end 
+    return word_score
+
 end
 
-
-
-end
+#=======================================================================
+# def self.score(word)
+#     value_array = []
+#    x = (word).split("")
+#     x.each do |letter|
+#         value_array << letter_value(letter)
+#     end
+#       total = value_array.inject(:+)
+#       return total
+# end
