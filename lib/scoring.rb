52c65a9dde62d8c4c2abd43618f7543c00da016c
justine <justine.winnie@gmail.com>
2016-03-08 13:53:09 -0800

    Restructured SCORE_CHART hash, created method to find the points for a given letter, refactored tests accordingly.

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -3,14 +3,33 @@ require_relative '../scrabble'
 class Scrabble::Scoring
 
   SCORE_CHART = {
-    1 => %w[A E I O U L N R S T],
-    2 => %w[D G],
-    3 => %w[B C M P],
-    4 => %w[F H V W Y],
-    5 => ["K"],
-    8 => %w[J X],
-    10 => %w[Q Z]
-  }
+"A" => 1,
+"E" => 1,
+"I" => 1,
+"O" => 1,
+"U" => 1,
+"L" => 1,
+"N" => 1,
+"R" => 1,
+"S" => 1,
+"T" => 1,
+"D" => 2,
+"G" => 2,
+"B" => 3,
+"C" => 3,
+"M" => 3,
+"P" => 3,
+"F" => 4,
+"H" => 4,
+"V" => 4,
+"W" => 4,
+"Y" => 4,
+"K" => 5,
+"J" => 8,
+"X" => 8,
+"Q" => 10,
+"Z" => 10
+}
 
   def self.score(word)
       puts score
@@ -27,11 +46,8 @@ class Scrabble::Scoring
 
   end
 
-
-  # we want this method to return the key not the key/value pair
-
-  def self.search_score_chart(word)
-    SCORE_CHART.select{ |value, letters| letters.include?(word) }
+  def self.letter_points(letter)
+    SCORE_CHART.fetch(letter)
   end
 
 
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -5,24 +5,20 @@ require_relative './spec_helper'
 require_relative '../lib/scoring'
 # require_relative '../scrabble'
 
-SCORE_CHART_TEST = [
-Scrabble::Scoring::SCORE_CHART[1].include?("A")
-]
-
 describe Scrabble::Scoring do
   it "exists" do
     Scrabble::Scoring.wont_be_nil
   end
 
   describe Scrabble::Scoring::SCORE_CHART do
-    it "includes 'A'" do
-      assert_equal true, SCORE_CHART_TEST[0]
+    it "corresponds to the correct points" do
+      assert_equal true, Scrabble::Scoring::SCORE_CHART["A"] == 1
     end
   end
 
-  describe "Scoring#search_score_chart" do
-    it "should return correct key value pair for the input letter" do
-      assert_equal Scrabble::Scoring::SCORE_CHART[1], Scrabble::Scoring.search_score_chart("O")
+  describe "Scoring#letter_points" do
+    it "should return points for given letter" do
+      assert_equal Scrabble::Scoring::SCORE_CHART["A"], Scrabble::Scoring.letter_points("A")
     end
 
   end
