7c6dce7ba92b5d785f5cf7d00f25b76cf058d42a
Alysia Brown <alysiacbrown@gmail.com>
2016-03-07 16:42:52 -0800

    Created specs folder.

---
+++ specs/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'minitest'
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+#give us some really pretty output :)
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
