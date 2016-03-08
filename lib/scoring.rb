a002da20eef6a5f4c72e53626ebd0660880a55fd
justine <justine.winnie@gmail.com>
2016-03-08 09:58:52 -0800

    Added constant hash of scoring values.

---
+++ lib/scoring.rb
@@ -0,0 +1,14 @@
+class Scoring
+
+  SCORE_CHART = {
+    1 => %w[A E I O U L N R S T],
+    2 => %w[D G],
+    3 => %w[B C M P],
+    4 => %w[F H V W Y],
+    5 => ["K"],
+    8 => %w[J X],
+    10 => %w[Q Z]
+  }
+
+
+end
