8641558e46417d69f1940396527e9bb48edfe604
Suzanne Harrison <harrison_family@hotmail.com>
2016-03-08 10:56:24 -0800

    added to score method in scrabble.rb but now need to start putting this class Scoring into the scroring.rb

--- scrabble.rb
+++ scrabble.rb
@@ -2,16 +2,23 @@ module Scrabble
   class Scoring
     #some sort of data structure to store the individual letter scores
   	SCORE_CHART = {
-  		1 => ["a", "e", "i", "u", "o", "l", "n", "r", "s", "t"]
-  		2 => ["d", "g"]
-  		3 => ["b", "c", "m", "p"]
-  		4 => ["f", "h", "v", "w", "y"]
-  		5 => ["k"]
-  		8 => ["j", "x"]
-  		10 => ["q", "z"]
-   	}
+      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+      "Y"=>4, "Z"=>10
+    }
   	def self.score(word)
-  		#returns score value for given word. word input as string.
+  		word_array = word.split("").upcase
+
+      word_score = 0
+
+      word_array.each do |letter|
+      word_score += SCORE_CHART[letter]
+      #returns score value for given word. word input as string.
+      #word is input as a string (case insensitive).
   		#seven letter word get 50point bonus
   	end
 
