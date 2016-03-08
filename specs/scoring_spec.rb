f59f9b7c497d9141c3516063aa323abf8ca642c8
Jade Vance <jadenicolevance@gmail.com>
2016-03-08 09:53:32 -0800

    Completed baseline requirements

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -1,4 +1,9 @@
 require_relative './spec_helper'
-require_relative '../scrabble'
+require_relative '../lib/scoring'
 
 # Need to write a test that turns red. Maybe "contains a scoring class" test
+describe Scoring do 
+	it "is an object we have access to" do
+		Scoring.wont_be_nil
+	end
+end
