2ebf6a4ccf17ce8430651980bc4a5dbf9df30665
Rowan Cota <cota.rg@gmail.com>
2016-03-08 10:59:35 -0800

    commiting and pushing to trade drivers

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -3,6 +3,11 @@ require_relative '../scrabble'
 require_relative '../lib/scoring'
 
 describe Scoring do
+	it "exists" do
+		Scoring.wont_be_nil
+	end
+
+
 	it "returns total score value" do
 		Scoring.score("sat").must_equal(3)
 	end
