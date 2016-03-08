2dc2c3b140af0af7f07fb484723701cd4d1024af
Jade Vance <jadenicolevance@gmail.com>
2016-03-08 10:51:27 -0800

    Wrote a test for 50 point bonus in scoring class method. Also wrote a test to add that score to the total score and return correctly.

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,11 +1,64 @@
 class Scoring
-  SCORING = {
-    1   =>  %w(A, E, I, O, U, L, N, R, S, T),
-    2   =>  %w(D, G),
-    3   =>  %w(B, C, M, P),
-    4   =>  %w(F, H, V, W, Y),
-    5   =>  %w(K),
-    8   =>  %w(J, X),
-    10  =>  %w(Q, Z)
+  LETTER_SCORING = {
+  #   1   =>  %w(A, E, I, O, U, L, N, R, S, T),
+  #   2   =>  %w(D, G),
+  #   3   =>  %w(B, C, M, P),
+  #   4   =>  %w(F, H, V, W, Y),
+  #   5   =>  %w(K),
+  #   8   =>  %w(J, X),
+  #   10  =>  %w(Q, Z)
+
+    "A" => 1,
+    "B" => 3,
+    "C" => 3,
+    "D" => 2,
+    "E" => 1,
+    "F" => 4,
+    "G" => 2,
+    "H" => 4,
+    "I" => 1,
+    "J" => 8,
+    "K" => 5,
+    "L" => 1,
+    "M" => 3,
+    "N" => 1,
+    "O" => 1,
+    "P" => 3,
+    "Q" => 10,
+    "R" => 1,
+    "S" => 1,
+    "T" => 1,
+    "U" => 1,
+    "V" => 4,
+    "W" => 4,
+    "X" => 8,
+    "Y" => 4,
+    "Z" => 10,
   }
-end #class end
+
+  def self.score(word)
+
+	  word_array = word.split
+	  score_value = 0
+
+	  amount_of_loops = word_array.length
+	   if amount_of_loops == 7
+	     score_value +=50
+	   end
+
+	   word_array.each do |letter|
+	     LETTER_SCORING.each do |hash_letter, point_value|
+		     if letter == hash_letter
+		       score_value += point_value
+		     end
+		   end 
+	     return score_value
+	   end
+  end
+
+  def self.highest_score_from(array_of_words)
+
+  end
+
+end 
+	
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -1,9 +1,22 @@
 require_relative './spec_helper'
 require_relative '../lib/scoring'
 
-# Need to write a test that turns red. Maybe "contains a scoring class" test
+#
+# Need a minimum of 8 specs for the scoring class 
+#
+
 describe Scoring do 
 	it "is an object we have access to" do
 		Scoring.wont_be_nil
 	end
+
+	it "is a class method we have access to" do 
+		Scoring.score("test").wont_be_nil
+	end 
+
+	it "validate fifty point bonus for using all seven tiles in a turn" do 
+	# require = 66 (50 + 16)
+		Scoring.score("REQUIRE")
+				return 66 
+		end 
 end
