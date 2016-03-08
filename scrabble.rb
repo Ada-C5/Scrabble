c47469b04cf9dce6ee79dba3772afb213f3dbbf7
Rowan Cota <cota.rg@gmail.com>
2016-03-08 13:26:51 -0800

    required relative scoring.rb.

--- scrabble.rb
+++ scrabble.rb
@@ -3,12 +3,12 @@ require_relative './lib/scoring'
 module Scrabble
 
 # fill in this array of hashes as a lookup table
-LETTER_POINT_VALUES = [[1, "a", "e", "i", "o", "u", "l", "n", "r", "s", "t"], 
-												[2, "d", "g"], 
-												[3, "b", "c", "m", "p"],
-												[4, "f", "h", "v", "w", "y"],
-												[5, "k"],
-												[8, "j", "x"],
-												[10, "q", "z"]]
+LETTER_POINT_VALUES = { 1: :["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"], 
+												2: :["d", "g"], 
+												3: :["b", "c", "m", "p"],
+												4: :["f", "h", "v", "w", "y"],
+												5: :["k"],
+												8: :["j", "x"],
+												10 :["q", "z"]}
 
 end
