49510c9227079c0a92f11d8c43237e9375cc14d3
Sarah Nahmias <nahmisa@gmail.com>
2016-03-08 13:36:30 -0800

    Added TEST_CASES hash for score method and it passed.  We rule.

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -4,7 +4,57 @@ require_relative '../lib/scoring'
 describe Scoring do
   it "is an object we have access to" do
     Scoring.wont_be_nil
-    
   end
+
+  describe "Scoring#score" do
+    it "is an object we have access to" do
+      Scoring.score("").wont_be_nil
+    end
+
+    TEST_CASES = {
+       1 => "a", #easiest
+       5 => "cat", #easier
+       4 => "rail",
+       12 => "zoo",
+       34 + 50 => "jacuzzi" #should have 50 point bonus for 7 letters
+    }
+    TEST_CASES.each do |word_score, word|
+     it "should return the score '#{ word_score }' for the word #{ word }" do
+       Scoring.score(word).must_equal(word_score)
+     end
+   end
+
+  end
+
+
+  describe "Scoring#highest_score_from" do
+    it "is an object we have accress to" do
+      Scoring.highest_score_from(["string", "string"]).wont_be_nil
+    end
+
+  end
+
+
+
+
+    #  TEST_CASES = {
+    #    1 => "I",
+    #    5 => "V",
+    #    10 => "X",
+    #    2 => "II",
+    #    15 => "XV",
+    #    4 => "IV",
+    #    9 => "IX",
+    #    14 => "XIV",
+    #    30 => "",
+    #    29 => "XXIX",
+    #    34 => "XXXIV",
+    #    49 => "XLIX",
+    #    74 => "LXXIV"
+    #  }
+    #TEST_CASES.each do |arabic, roman|
+    #  it "should return the roman '#{ roman }' for the arabic #{ arabic }" do
+    #    Roman.convert(arabic).must_equal(roman)
+    #  end
 
 end
