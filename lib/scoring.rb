fdd70868b3457d124570fbbe6dc75256ac15b654
Yordanos <yordanosd@gmail.com>
2016-03-08 13:35:00 -0800

    We added the score test for Scrabble::Score.score and added self.score method to scoring.rb. We successfully tested single letters.

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,6 +1,25 @@
-require_relative '../scrabble'
+# require_relative '../scrabble'  #   allows this class to access scrabble and run through irb
 
 class Scrabble::Scoring
   include Scrabble
+  LETTER_POINTS = {
+    1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+    2 => ["D", "G"],
+    3 => ["B", "C", "M", "P"],
+    4 => ["F", "H", "V", "W", "Y"],
+    5 => ["K"],
+    8 => ["J", "X"],
+    10 => ["Q", "Z"]
+  }
 
+  def self.score(word)
+    score = nil
+
+    LETTER_POINTS.each do |points, letters_array|
+      if letters_array.include? word.upcase
+        score = points
+      end
+    end
+    score
+  end
 end
--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -1,8 +1,30 @@
 require_relative './spec_helper'
-require_relative '../scrabble'
+
 
 describe Scrabble::Scoring do
   it "is an object we have access to" do
     Scrabble::Scoring.wont_be_nil
   end
+end
+
+describe "Scrabble::Score.score" do
+  # TEST_SCRABBLE_SCORE = {
+  #   1 => [A, E, I, O, U, L, N, R, S, T],
+  #   2 => [D, G],
+  #   3 => [B, C, M, P],
+  #   4 => [F, H, V, W, Y],
+  #   5 => [K],
+  #   8 => [J, X],
+  #   10 => [Q, Z]
+  # }
+
+  it "should return the scrabble score of 1 for the letter 'A' " do
+    Scrabble::Scoring.score("A").must_equal (1)
+  end
+
+  it "should return the scrabble score of 3 for the letter 'M' " do
+    Scrabble::Scoring.score("M").must_equal (3)
+  end
+
+
 end
