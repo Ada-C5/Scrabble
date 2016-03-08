4085b3eff06e41aeb8c38bd713f34c084abb6a2a
sophia <sophiabaldonado@gmail.com>
2016-03-08 11:03:49 -0800

    Created search_score_chart method that returns the key value pair for a searched letter;

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,4 +1,6 @@
-class Scoring
+require_relative '../scrabble.rb'
+
+class Scrabble::Scoring
 
   SCORE_CHART = {
     1 => %w[A E I O U L N R S T],
@@ -9,6 +11,25 @@ class Scoring
     8 => %w[J X],
     10 => %w[Q Z]
   }
+
+  def self.score(word)
+      puts score
+  end
+
+  def word_letters(word)
+     word.split(//)
+  end
+
+  def finding_values(word)
+
+      # see what the letter's key is and return all keys in a new array
+      word_letters.map { |letter| letter == SCORE_CHART[] }
+
+  end
+
+  def search_score_chart(word)
+    SCORE_CHART.select{ |value, letters| letters.include?(word) }
+  end
 
 
 end
