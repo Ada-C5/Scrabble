c7006d8483718792e27bb3dd9970c619e0c5a60c
Lisa Rolczynski <lisa.sano@gmail.com>
2016-03-08 10:56:21 -0800

    test to check that we can create array of letters from a given word

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -11,16 +11,18 @@ class Scoring
 
   def self.score(word)
     word_score = 0
-    letter_array = word.upcase.split
-    letter_array.each do |letter|
-      LETTER_SCORES.each do |key, value|
-        if value.include? letter
-          word_score += key
-        end
-      end
-    end
+    word = word.upcase
+    letter_array = word.split(//)
+    return letter_array
+    # letter_array.each do |letter|
+    #   LETTER_SCORES.each do |key, value|
+    #     if value.include? letter
+    #       word_score += key
+    #     end
+    #   end
+    # end
 
-    word_score
+    # word_score
   end
 
 end
--- specs/scoring-spec.rb
+++ specs/scoring-spec.rb
@@ -6,4 +6,14 @@ describe Scoring do
     Scoring.wont_be_nil
   end
 
+  describe "Scoring#score" do
+    # TEST_CASES = {
+    #   "cat" => 5
+    # }
+
+    it "should return ['C', 'A', 'T'] for the word cat" do
+      Scoring.score("cat").must_equal(['C', 'A', 'T'])
+    end
+  end
+
 end
