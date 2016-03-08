3281ee9b0c290a1c7a6088116ff014be070c316e
justine <justine.winnie@gmail.com>
2016-03-08 10:09:15 -0800

    Created passing test for SCORE_CHART hash.

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -1,2 +1,22 @@
+require 'simplecov'
+SimpleCov.start
+
 require_relative './spec_helper'
 require_relative '../lib/scoring'
+
+SCORE_CHART_TEST = [
+Scoring::SCORE_CHART[1].include?("A")
+]
+
+describe Scoring do
+  it "exists" do
+    Scoring.wont_be_nil
+  end
+
+  describe Scoring::SCORE_CHART do
+    it "includes 'A'" do
+      assert_equal true, SCORE_CHART_TEST[0]
+    end
+  end
+
+end
--- specs/scrabble_spec.rb
+++ specs/scrabble_spec.rb
@@ -1,2 +1,5 @@
+require 'simplecov'
+SimpleCov.start
+
 require_relative './spec_helper'
 require_relative '../scrabble'
