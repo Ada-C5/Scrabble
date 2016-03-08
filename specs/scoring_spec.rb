200726e120faeb728d084ce2d1acfc88419c3ca4
Nadine <nlcurry@gmail.com>
2016-03-08 10:22:25 -0800

    Added 2 tests on scoring_spec and score chart and first two methods in wave one for file scrabble

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -5,4 +5,14 @@ describe Scrabble::Scoring do
   it "it is an object we have acccess to" do
     Scrabble::Scoring.wont_be_nil
   end
+
+  TEST_SCORE = {
+  	"zebra" => 16
+  }
+
+  TEST_SCORE.each do |word, score|
+  	it "should return score for the word" do
+  	Scoring.score(word).must_equal(score)
+  	end
+  end
 end
