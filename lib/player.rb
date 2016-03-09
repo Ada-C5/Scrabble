0e09d559f558067542e9790062b903c881947f99
Sarah <sarahkane682@yahoo.com>
2016-03-09 10:30:56 -0800

    added play(word) method, and total_score method

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,19 @@
 class Player
-  attr_reader :name
+  require_relative './scoring'
+  attr_reader :name, :plays ,:total_score
   def initialize(name)
     @name = name
-
+    @plays = []
+    @total_score = 0
   end
+
+
+  def play(word)
+    @plays << word
+    score = Scoring.score(word)
+    @total_score += score
+    score
+  end
+
 
 end
--- specs/player-spec.rb
+++ specs/player-spec.rb
@@ -2,6 +2,11 @@ require_relative './spec_helper'
 require_relative '../lib/player'
 
 describe Player do
+  before do
+    @sarah = Player.new("sarah")
+  end
+
+
     it "is an object we have access to" do
     Player.wont_be_nil
 
@@ -9,17 +14,33 @@ describe Player do
 
   describe "Player#name" do
     it "returns sarah when initialized with sarah" do
-      Player.new("sarah").name.must_equal "sarah"
+      @sarah.name.must_equal "sarah"
 
     end
   end
 
-  # describe "Player#plays" do
-  #   it "returns an Array of words played by the player" do
-  #     Player.new.
+describe "words played by player" do
+  before do
+    @sarah.play("cat")
+  end
 
-  #   end
 
-  # end
+    it "returns the score of the word for the play(word) method" do
+      @sarah.play("cat").must_equal 5
+    end
 
+
+     it "returns an array of words gussed for the plays method" do
+       @sarah.plays.must_equal ["cat"]
+
+     end
+
+
+
+     it "returns the total score of all words played for the total_score method" do
+       @sarah.total_score.must_equal 5
+     end
+
+
+   end
 end
