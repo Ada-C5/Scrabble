24fbbf69d4a6d8127fede3d30d05b0f73808be57
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-03-09 10:58:31 -0800

    Wave 2 complete. 11 tests for primary requirements and complete player class functionality including: name, plays, play(word), total score, won method, highest scoring word method, and highest word score method. WE ARE AWESOME

---
+++ lib/player.rb
@@ -0,0 +1,37 @@
+require_relative './scoring.rb'
+
+class Player
+  attr_reader :name
+  attr_accessor :plays
+  def initialize(name)
+    @name = name
+    @plays = []
+  end
+
+  def play(word)
+    if won?
+      return false
+    else
+      plays << word
+      return Scoring.score(word) #make sure pulling scoring from scoring.rb
+    end
+  end
+
+  def total_score
+    (return 0) if plays.empty?
+    score_array = plays.collect { |word| Scoring.score(word)}
+    score_array.reduce(:+)
+  end
+
+  def won?
+    total_score > 100 ? true : false
+  end
+
+  def highest_scoring_word
+    Scoring.highest_score_from(plays)
+  end
+
+  def highest_word_score
+    Scoring.score(highest_scoring_word)
+  end
+end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -1,2 +1,78 @@
 require_relative './spec_helper'
 require_relative '../lib/player'
+
+
+describe Player do
+  it "has a name" do
+    Player.name.wont_be_nil
+  end
+
+  it "returns score for word played" do
+    Player.new("game").play("cat").must_equal(5)
+  end
+
+  it "adds play to plays array" do
+    play_cat = Player.new("game")
+    play_cat.play("cat")
+    play_cat.plays.must_equal(["cat"])
+  end
+
+  it "returns sum of scores of played words" do
+    play_cat = Player.new("game")
+    play_cat.play("cat")
+    play_cat.play("dog")
+    play_cat.total_score.must_equal(10)
+  end
+
+  it "wins if player has over 100 points" do
+    play_cat = Player.new("game")
+    play_cat.play("jacuzzi")
+    play_cat.play("quizzed")
+    play_cat.won?.must_equal(true)
+  end
+
+  it "returns highest scoring word played" do
+    play_cat = Player.new("game")
+    play_cat.play("jazzily")
+    play_cat.play("quizzed")
+    play_cat.highest_scoring_word.must_equal("jazzily")
+  end
+
+  it "returns highest word score" do
+    play_cat = Player.new("game")
+    play_cat.play("jacuzzi")
+    play_cat.play("quizzed")
+    play_cat.highest_word_score.must_equal(85)
+  end
+
+  it "adds multiple plays to plays array" do
+    play_cat = Player.new("game")
+    play_cat.play("cat")
+    play_cat.play("porcupine")
+    play_cat.play("lol")
+    play_cat.plays.must_equal(["cat", "porcupine", "lol"])
+  end
+
+  it "returns false if player has less than 100 points" do
+    play_cat = Player.new("game")
+    play_cat.play("cat")
+    play_cat.play("quizzed")
+    play_cat.won?.must_equal(false)
+  end
+
+  it "player has already won: play return false" do
+    play_cat = Player.new("game")
+    play_cat.play("jacuzzi")
+    play_cat.play("quizzed")
+    play_cat.play("cat").must_equal(false)
+  end
+
+  it "player has already won: play return false" do
+    play_cat = Player.new("game")
+    play_cat.play("jacuzzi")
+    play_cat.play("quizzed")
+    play_cat.play("cat")
+    play_cat.plays.must_equal(["jacuzzi", "quizzed"])
+  end
+
+end
