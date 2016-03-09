da23458c5d087f818877c9297410308c7980ee2c
Sarah Nahmias <nahmisa@gmail.com>
2016-03-09 15:55:05 -0800

    Added TileBag class and test for existence of class

---
+++ lib/tilebag.rb
@@ -0,0 +1,3 @@
+class TileBag
+
+end
---
+++ specs/tilebag_spec.rb
@@ -0,0 +1,14 @@
+require_relative './spec_helper'
+require_relative '../lib/tilebag'
+
+describe TileBag do
+  it "is an object we have access to" do
+    TileBag.wont_be_nil
+  end
+
+  # describe "Scoring#score" do
+  #   it "is an object we have access to" do
+  #     Scoring.score("").wont_be_nil
+  #  end
+
+end
