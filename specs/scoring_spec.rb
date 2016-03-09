7e338066f562a9ed1eba0f8dedc3fa815cd394ca
Jade Vance <jadenicolevance@gmail.com>
2016-03-09 15:54:18 -0800

    Added in module scrabble.

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -1,7 +1,7 @@
 require_relative './spec_helper'
 require_relative '../lib/scoring'
 
-describe Scoring do
+describe Scrabble::Scoring do
 	# Test 5
 	HIGH_SCORE = ["dog", "bird", "quiz", "bug"]
 	# Test 6
@@ -13,41 +13,41 @@ describe Scoring do
 
 	# TEST 1
 	it "is an object we have access to" do
-		Scoring.wont_be_nil
+		Scrabble::Scoring.wont_be_nil
 	end
 	# TEST 2
 	it "is a class method we have access to" do
-		Scoring.score("test").wont_be_nil
+		Scrabble::Scoring.score("test").wont_be_nil
 	end
 	# TEST 3
 	it "validates score method returns correct score" do
-		Scoring.score("pig")
+		Scrabble::Scoring.score("pig")
 			return 6
 		end
 	# TEST 4
 	it "50-point bonus is applied to words >= 7 letters" do
-		Scoring.score("require")
+		Scrabble::Scoring.score("require")
 			return 66
 	end
 	# TEST 5
 	it "returns the highest scoring word" do
-		Scoring.highest_score_from(HIGH_SCORE)
+		Scrabble::Scoring.highest_score_from(HIGH_SCORE)
 		 return "quiz"
 	end
 	# TEST 6
 	it "returns highest scoring word with 50 point bonus" do
-		Scoring.highest_score_from(FIFTY_POINT_HIGH_SCORE)
+		Scrabble::Scoring.highest_score_from(FIFTY_POINT_HIGH_SCORE)
 		 return "require"
 	end
 	# TEST 7
 	it "returns the word with the fewest letters during a tie" do
-		Scoring.highest_score_from(RETURN_FEWEST_LETTERS_TIE)
+		Scrabble::Scoring.highest_score_from(RETURN_FEWEST_LETTERS_TIE)
 		return "zinc"
 	end
 	# TEST 8
 	# for highest score and fewest letters
 	it "returns first given word during a tie" do
-		Scoring.highest_score_from(RETURN_FIRST_WORD_TIE)
+		Scrabble::Scoring.highest_score_from(RETURN_FIRST_WORD_TIE)
 		return "zaps"
 	end
 end
