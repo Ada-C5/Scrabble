bc3a04d8b045221995e374522aed265bdd6384ec
Jade Vance <jadenicolevance@gmail.com>
2016-03-09 10:08:44 -0800

    Cleaned up and clarified scoring_spec tests, arranged constants to be reflective of the test they are performing

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -7,46 +7,51 @@ require_relative '../lib/scoring'
 
 describe Scoring do
 
-	ARRAY_OF_WORDS = ["dog", "bird", "quiz", "require"]
+	HIGH_SCORE = ["dog", "bird", "quiz", "bug"] # Test 5
+	
+	FIFTY_POINT_HIGH_SCORE  = ["dog", "bird", "quiz", "require"] # Test 6
 
-	# zaps and squad are both worth 15 points
-	TIE_BREAKER_WORDS = ["ale", "hi", "zinc", "cat", "squad", "butts", "zaps"]
+	RETURN_FEWEST_LETTERS_TIE = ["ale", "hi", "zinc", "cat", "squad", "butts"] # Test 7 
 
+	RETURN_FIRST_WORD_TIE = ["ale", "hi", "zaps", "cat", "squad", "butts", "zinc"] # Test 8 
+
+	# TEST 1
 	it "is an object we have access to" do
 		Scoring.wont_be_nil
 	end
-
+	# TEST 2
 	it "is a class method we have access to" do
 		Scoring.score("test").wont_be_nil
 	end
-
-	it "validates scoring is correct" do
+	# TEST 3
+	it "validates score method returns correct score" do
 		Scoring.score("pig")
 			return 6
 		end
-
-	it "validates 50-point bonus is applied" do
+	# TEST 4
+	it "50-point bonus is applied to words >= 7 letters" do
 		Scoring.score("require")
 			return 66
 	end
-
+	# TEST 5
 	it "returns the highest scoring word" do
-		Scoring.highest_score_from(ARRAY_OF_WORDS)
+		Scoring.highest_score_from(HIGH_SCORE)
 		 return "quiz"
 	end
-
-	it "returns the highest scoring word" do
-		Scoring.highest_score_from(ARRAY_OF_WORDS)
+	# TEST 6
+	it "returns highest scoring word with 50 point bonus" do
+		Scoring.highest_score_from(FIFTY_POINT_HIGH_SCORE)
 		 return "require"
 	end
-
+	# TEST 7
 	it "returns the word with the fewest letters during a tie" do
-		Scoring.highest_score_from(TIE_BREAKER_WORDS)
+		Scoring.highest_score_from(RETURN_FEWEST_LETTERS_TIE)
 		return "ZINC"
 	end
-
-	it "returns the first given word during a tie for highest score and fewest letters" do
-		Scoring.highest_score_from(["ale", "hi", "zaps", "cat", "squad", "butts", "zinc"])
+	# TEST 8
+	# for highest score and fewest letters
+	it "returns first given word during a tie" do
+		Scoring.highest_score_from(RETURN_FIRST_WORD_TIE)
 		return "ZAPS"
 	end
 
