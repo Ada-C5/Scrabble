b0961a657957f103fcc9cc7c83fa3bc9287b1ec7
Sarah Nahmias <nahmisa@gmail.com>
2016-03-08 16:33:10 -0800

    Reordered TEST CASES so they increase in complexity

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -24,7 +24,7 @@ describe Scoring do
       4 => "rail",
       12 => "zoo"
     }
-    
+
     TEST_CASES_SCORE.each do |word_score, word|
      it "should return the score '#{ word_score }' for the word #{ word }" do
        Scoring.score(word).must_equal(word_score)
@@ -38,12 +38,12 @@ describe Scoring do
     end
 
     TEST_CASES_ARRAY = {
-       "jacuzzi" => ["cat", "rail", "zoo", "jacuzzi"], # tested single 7 letter word
-       "cat" => ["cat", "dog", "god", "met"],
-       "k" => ["cat", "dog", "god", "met", "k"], # k also has score of 5 but wins by shortest letters
-       "zizzled" => ["zizzled", "quizzed", "cat"], # case of multiple 7 letter tie
-       "quizzed" => ["quizzed", "wat"] #considered testing case of shorter word having greater value than 7 letter, near impossible to test
-
+      "dog" => ["dog"], # does a one-word array work?
+      "jacuzzi" => ["cat", "rail", "zoo", "jacuzzi"], # tested single 7 letter word
+      "quizzed" => ["quizzed", "wat"], #considered testing case of shorter word having greater value than 7 letter, near impossible to test
+      "k" => ["cat", "dog", "god", "met", "k"], # tied scores, but shortestwins
+      "cat" => ["cat", "dog", "god", "met"], # tied scores, same length - first listed should win
+      "zizzled" => ["zizzled", "quizzed", "cat"] # case of multiple 7 letter tie (same score, same length, first listed should win... make sure it works with seven letters!)
     }
     TEST_CASES_ARRAY.each do |winning_word, array_words|
      it "should return the winning word '#{ winning_word }' for the array of words #{ array_words }" do
