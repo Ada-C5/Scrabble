27dd6ad646933bffb69ae2b9441b73d330a9caec
Jade Vance <jadenicolevance@gmail.com>
2016-03-09 16:03:09 -0800

    Roll back of changes due to confusion

--- lib/player.rb
+++ lib/player.rb
@@ -1,45 +1,43 @@
 require_relative '../lib/scoring'
 
-module Scrabble
-	class Player
-	  def initialize(name)
-	    @name = name
-	    @words_played = []
-	    @scores_array = []
-	  end
+class Player
+  def initialize(name)
+    @name = name
+    @words_played = []
+    @scores_array = []
+  end
 
-	  def name_return
-	    @name
-	  end
+  def name_return
+    @name
+  end
 
-	  def plays   #returns an array of all the words played by Player
-	    return words_played
-	  end
+  def plays   #returns an array of all the words played by Player
+    return words_played
+  end
 
-	  def play(word)  #adds word to the words_played array
-	    @words_played << word
-	    @scores_array << Scoring.score(word)
-	    # won?
-	  end
+  def play(word)  #adds word to the words_played array
+    @words_played << word
+    @scores_array << Scoring.score(word)
+    # won?
+  end
 
-	  def total_score #returns sum of scores of played words
+  def total_score #returns sum of scores of played words
 
 
-	  end
+  end
 
-	  def won?  #returns true if over 100pts, otherwise false(haven't won yet)
-	    if Player.total_score >= 100
-	      "You win!"
-	    else
-	      "You haven't won yet."
-	    end
-	  end
+  def won?  #returns true if over 100pts, otherwise false(haven't won yet)
+    if Player.total_score >= 100
+      "You win!"
+    else
+      "You haven't won yet."
+    end
+  end
 
-	  def highest_scoring_word #returns highest scoring word from words_played array
-	  end
+  def highest_scoring_word #returns highest scoring word from words_played array
+  end
 
-	  def highest_word_score #returns score of highest scoring word
-	  end
+  def highest_word_score #returns score of highest scoring word
+  end
 
-	end
-end 
+end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -1,2 +1,10 @@
 require_relative './spec_helper'
 require_relative '../lib/player'
+
+describe Scrabble::Player do 
+	# TEST 1
+	it "is an object we have access to" do
+		Scrabble::Player.wont_be_nil
+	end
+
+end
