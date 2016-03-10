5a23572edb354e6e89c416681826e36cca97527c
Risha <rishallen@gmail.com>
2016-03-09 17:13:29 -0800

    worte a test. will continue to work on it.

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,17 @@
 class Scrabble::Player
-  PLAYED_WORDS = []
+  attr_accessor :played_words
   def initialize(name)
     @name = name
+    @played_words = []
   end
 
   def plays
-    return PLAYED_WORDS
+    return @played_words
   end
+
+  def play(word)
+    @played_words << word
+    return false
+  end
+
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -8,14 +8,25 @@ describe Scrabble::Player do
   end
 end
 
-describe "InitializeInstanceVariable" do
-  it "should create instance of player name" do
-    Scrabble::Player.new("Tim").must_be_instance_of(Scrabble::Player)
+  describe "InitializeInstanceVariable" do
+    it "should create instance of player name" do
+      Scrabble::Player.new("Tim").must_be_instance_of(Scrabble::Player)
+    end
   end
-end
 
-describe "ReturnWordArray" do
-  it "should return an Array of the words played by player" do
-    Scrabble::Player.new("Tim").plays.must_equal([])
+  describe "ReturnWordArray" do
+    it "should return an Array of the words played by player" do
+      Scrabble::Player.new("Tim").plays.must_equal([])
+    end
   end
-end
+
+  describe "PlayWordsMethodExists" do
+    it "should input a new word to the plays array" do
+      Scrabble::Player.new("risha").play("RED")
+      # played_words.include?("RED")
+    end
+
+    # it "should include word into an array" do
+    # played_words.include?("RED")
+    # end
+  end
