de4d364b992ef0df83fd88108ddb2f4b37842b50
justine <justine.winnie@gmail.com>
2016-03-09 17:04:37 -0800

    Created Player class with initialize(attr reader methods for name & words_play), won?, and play methods. Corresponding tests.

---
+++ lib/player.rb
@@ -0,0 +1,34 @@
+#name: returns the value of the @name instance variable
+#play(word): Adds the input word to the plays Array
+  # Returns false if player has already won
+  # Returns the score of the word
+#plays: returns an Array of the words played by the player
+#total_score: Returns the sum of scores of played words
+#won?: If the player has over 100 points, returns true, otherwise returns false
+#highest_scoring_word: Returns the highest scoring played word
+#highest_word_score: Returns the highest_scoring_word score
+
+require_relative '../scrabble'
+
+class Scrabble::Player
+  include Scoring
+  attr_reader :name, :words_played
+
+  def initialize(name)
+    @name = name
+    @words_played = []
+  end
+
+  def won?
+    false
+  end
+
+  def play(word)
+    # adds word to array of words
+    @words_played << word
+    # Returns false if player has already won
+    word_score = Scrabble::Scoring.score(word)
+      won? ? false : word_score
+      # scores word & returns score
+  end
+end
---
+++ specs/player_spec.rb
@@ -0,0 +1,23 @@
+require 'simplecov'
+SimpleCov.start
+
+require_relative './spec_helper'
+require_relative '../lib/player'
+
+describe Scrabble::Player do
+DORIS = Scrabble::Player.new("Doris")
+  it "exists" do
+    Scrabble::Player.wont_be_nil
+  end
+
+  it "should require a name parameter" do
+    assert_equal "Doris", DORIS.name
+  end
+
+  describe "Scrabble::Player#play(word)" do
+    it "should push the played word into the words_played array" do
+      DORIS.play("dumpling")
+      assert DORIS.words_played.include?("dumpling")
+    end
+  end
+end
