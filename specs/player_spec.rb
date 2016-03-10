3d6cf86a1732405ceb1b6a21cd39bd270e14319a
Rowan Cota <cota.rg@gmail.com>
2016-03-10 13:42:06 -0800

    Committing specifically to revert to previous version.

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -20,48 +20,51 @@ describe Scrabble::Player do
 		Scrabble::Player.wont_be_nil
 	end
 
-TEST_PLAYERS = [
-  @jane,
-  @bob,
-  @fred
-]
-
-TEST_PLAYERS.each do |player|
+  it "has a readable name instance variable" do
+    @jane.name.must_equal("Jane")
+  end
 
   it "will return a value when total score is queried" do
-  	player.total_score.wont_be_nil
+  	@jane.total_score.wont_be_nil
   end
 
   it "will return the score when a play is made" do
-    player.play("cat").must_equal(5)
+    @jane.play("cat").must_equal(5)
   end
 
   it "will increase total_score when a word is played" do
-    player.total_score.must_equal(5)
+    @jane.total_score.must_equal(5)
   end
 
   it "will push a word into the plays array" do
-    player.plays.must_include("cat")
+    @jane.plays.must_include("cat")
   end
 
   it "will return true if total score is >100" do
-    player.won?.must_equal(true)
+    @fred.won?.must_equal(true)
   end
 
-	it "returns highest scoring word in an array" do
-		player.highest_scoring_word(player.plays).must_equal("cupcake")
+  TEST_ARRAYS = {
+		["cat", "dog", "face", "data"] => "face",
+		["talon", "cat", "pie", "dog"] => "cat",
+		["jack", "benzol", "blazer", "cupcake"] => "cupcake"
+	}
+
+	TEST_ARRAYS.each do |array, word|
+		it "selects the highest value word in the array" do
+			Scrabble::Player.highest_scoring_word(array).must_equal(word)
+		end
+
+    it "selects the highest value word in the array and shows score value" do 
+      Scrabble::Player.highest_word_score.must_equal(word.score)
+    end
+
 	end
 
-  it "returns the score for highest scoring word" do
-    player.highest_word_score.must_equal(67)
-  end
 
-  it "returns false if fred tries to play" do
-    player.play("cat").must_equal(false)
-  end
 
   # it "will return false if the player has won" do
   #   @jane.total_score
   # end
-end
+
 end
