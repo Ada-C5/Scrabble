dd2426dd1cc0451d1b89e03583ae06c39ead4489
Adriana <dri19tcc@gmail.com>
2016-03-09 16:59:33 -0800

    Added extra tests to make sure all our methods would be there.  Started researching assert_not_nil.  Trying to get tests to fail since not existing yet

--- spec/player-spec.rb
+++ spec/player-spec.rb
@@ -14,11 +14,47 @@ describe Player do
 
   describe "Player#plays" do
     it "Does it exist" do
-      "Player#plays.wont_be_nil"
+      assert_nil(Player.plays, failure_message = nil)
     end
 
-    it "returns an array of the words played by the player" do
+    # it "returns an array of the words played by the player" do
+    #   assert Player.plays.include?([])
+    # end
+  end
+
+  describe "Player#plays" do
+    it "Does it exist plays" do
       "Player#plays.wont_be_nil"
+    end
+  end
+
+  describe "Player#play(word)" do
+    it "Does it exist play(word)" do
+      "Player#play(name).wont_be_nil"
+    end
+  end
+
+  describe "Player#total_score" do
+    it "Does it exist total_score" do
+      "Player#total_score.wont_be_nil"
+    end
+  end
+
+  describe "Player#won?" do
+    it "Does it exist won?" do
+      "Player#won?.wont_be_nil"
+    end
+  end
+
+  describe "Player#highest_scoring_word" do
+    it "Does it exist highest_scoring_word?" do
+      "Player#highest_scoring_word.wont_be_nil"
+    end
+  end
+
+  describe "Player#highest_word_score?" do
+    it "Does it exist highest_word_score" do
+      "Player#highest_word_score.wont_be_nil"
     end
   end
 end
