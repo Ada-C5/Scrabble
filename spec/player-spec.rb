adaa5ee6d0fd048c814a1f7a8c64fe49ed709e20
Jillian Boshart <Jilliliz@gmail.com>
2016-03-10 13:02:47 -0800

    Working on plays method, seeing if it returns an array.

--- spec/player-spec.rb
+++ spec/player-spec.rb
@@ -13,48 +13,54 @@ describe Player do
   end
 
   describe "Player#plays" do
-    it "Does it exist" do
-      assert_nil(Player.plays, failure_message = nil)
+    it "Does it contain an array" do
+      
     end
+  end
+end
+
+
+
+
+
 
     # it "returns an array of the words played by the player" do
     #   assert Player.plays.include?([])
     # end
-  end
+  # end
 
-  describe "Player#plays" do
-    it "Does it exist plays" do
-      "Player#plays.wont_be_nil"
-    end
-  end
-
-  describe "Player#play(word)" do
-    it "Does it exist play(word)" do
-      "Player#play(name).wont_be_nil"
-    end
-  end
-
-  describe "Player#total_score" do
-    it "Does it exist total_score" do
-      "Player#total_score.wont_be_nil"
-    end
-  end
-
-  describe "Player#won?" do
-    it "Does it exist won?" do
-      "Player#won?.wont_be_nil"
-    end
-  end
-
-  describe "Player#highest_scoring_word" do
-    it "Does it exist highest_scoring_word?" do
-      "Player#highest_scoring_word.wont_be_nil"
-    end
-  end
-
-  describe "Player#highest_word_score?" do
-    it "Does it exist highest_word_score" do
-      "Player#highest_word_score.wont_be_nil"
-    end
-  end
-end
+  # describe "Player#plays" do
+  #   it "Does the 'plays' method exist" do
+  #     Player.plays.wont_be_nil
+  #   end
+  # end
+  #
+  # describe "Player#play(word)" do
+  #   it "Does the play(word) method exist" do
+  #     "Player#play(name).wont_be_nil"
+  #   end
+  # end
+  #
+  # describe "Player#total_score" do
+  #   it "Does the total_score method exist" do
+  #     "Player#total_score.wont_be_nil"
+  #   end
+  # end
+  #
+  # describe "Player#won?" do
+  #   it "Does the won? method exist" do
+  #     "Player#won?.wont_be_nil"
+  #   end
+  # end
+  #
+  # describe "Player#highest_scoring_word" do
+  #   it "Does the highest_scoring_word method exist" do
+  #     "Player#highest_scoring_word.wont_be_nil"
+  #   end
+  # end
+  #
+  # describe "Player#highest_word_score?" do
+  #   it "Does the highest_word_score method exist" do
+  #     "Player#highest_word_score.wont_be_nil"
+  #   end
+  # end
