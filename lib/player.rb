14e93d64a2e780c0524cce74e04e115a0fc18181
Rowan Cota <cota.rg@gmail.com>
2016-03-09 16:00:19 -0800

    Scaffolded in methods using requirements for assignment, commented with understanding of how this should work.

--- lib/player.rb
+++ lib/player.rb
@@ -3,8 +3,36 @@ require_relative '../scrabble'
 class Scrabble::Player
 attr_reader :name, :plays, :score
 
-def initialize(name)
-  @name = name
-end
+	# set up with empty plays array and score as instance variable
+	def initialize(name)
+	  @name = name
+	end
+
+	def play(word): 
+		# checks if won
+		# Returns false if player has already won
+
+		# add score for word to @score	
+		# Adds the input word to the plays Array
+
+		# Returns the score of the word
+
+
+
+	end
+
+
+	def total_score: 
+	# Returns the sum of scores of played words
+	end
+
+	def won?: # If the player has over 100 points, returns true, otherwise returns false
+	end
+
+	def highest_scoring_word: # Returns the highest scoring played word
+	end
+
+	def highest_word_score: # Returns the highest_scoring_word score
+	end
 
 end
