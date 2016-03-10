386e482787a3a651a7b3287b31179aef068cc25a
Rowan Cota <cota.rg@gmail.com>
2016-03-09 16:57:11 -0800

    Tests to see if play method returns correct value, if total score updates, if plays array updates. Using jerk code for now, should be refined.

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -6,6 +6,9 @@ describe Scrabble::Player do
   before do
     @jane = Scrabble::Player.new("Jane")
     @jane.play("cat")
+
+    @bob = Scrabble::Player.new("bob")
+    @bob.total_score
   end
 
   it "exists" do
@@ -19,17 +22,23 @@ describe Scrabble::Player do
   it "will return a value when total score is queried" do
   	@jane.total_score.wont_be_nil
   end
-
-  it "will return a value when a play is made" do
-  	@jane.play("jack").wont_be_nil
-  end
-
+  
   it "will return the score when a play is made" do
     @jane.play("cat").must_equal(5)
+  end
+
+  it "will increase total_score when a word is played" do 
+    @jane.total_score.must_equal(5)
   end
 
   it "will push a word into the plays array" do
     @jane.plays.must_include("cat")
   end
+
+
+
+  # it "will return false if the player has won" do
+  #   @jane.total_score
+  # end
 
 end
