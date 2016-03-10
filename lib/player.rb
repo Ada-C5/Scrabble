0e9dad236b9fee3cd559577d49c3f2a7f44962f5
Valerie <val.conklin@gmail.com>
2016-03-09 16:51:16 -0800

    Added tests for the plays and play(word) methods and passed yaaay

--- lib/player.rb
+++ lib/player.rb
@@ -3,8 +3,17 @@
 class Scrabble::Player
   include Scrabble
   attr_reader :name
-  
+
   def initialize(player_info)
     @name = player_info[:name]
+    @words_played_array = []
+  end
+
+  def plays
+    @words_played_array
+  end
+
+  def play(word)
+    @words_played_array << word
   end
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -4,12 +4,29 @@ describe Scrabble::Player do
   it "should be an object we have access to" do
     Scrabble::Player.wont_be_nil
   end
+end
 
 describe "Scrabble::Player#name" do
+  # returns the value of the @name instance variable
   it "should return the value 'Joe' when player instance is named Joe" do
     joe = Scrabble::Player.new(name: "Joe")
     joe.name.must_equal ("Joe")
   end
-
 end
+
+describe "Scrabble::Player#plays and #play(word)" do
+  # returns an array of the words played by the player
+  joe = Scrabble::Player.new(name: "Joe")
+  it "should return [] played by Joe" do
+    # joe = Scrabble::Player.new(name: "Joe")
+    joe.plays.must_equal []
+  end
+
+  # adds input word to the plays array
+  it "should return ['Japan', 'Eritrea', 'Mexico'] when joe.play('Mexico')" do
+    joe.play("Japan")
+    joe.play("Eritrea")
+    joe.play("Mexico")
+    joe.plays.must_equal ["Japan", "Eritrea", "Mexico"]
+  end
 end
