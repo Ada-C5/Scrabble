5375fbe7caeb5bdeb304739c881f292b71779a47
Jade Vance <jadenicolevance@gmail.com>
2016-03-10 16:57:02 -0800

    Added skeleton to testing tilebag specs

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -4,9 +4,13 @@ require_relative '../lib/tilebag'
 describe TileBag do
 
 # TEST 1
+# Player has tile bag? 
 # TEST 2
+# Player has access to tiles? 
 # TEST 3
+# Correctly uses tiles from tiles in hand when played
 # TEST 4
+# Correctly removes tiles from bag when drawn
 # TEST 5
 	
 end
