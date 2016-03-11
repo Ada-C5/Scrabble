b358fd7401c226d95fdffd78ad04d2bda6198ebf
Deirdre Storck <deirdre.storck@gmail.com>
2016-03-11 10:16:57 -0800

    changed how the default tiles are initialized and now we are finding all kinds of problems iwth our tile removal

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -9,18 +9,17 @@ describe Scrabble::Tilebag do
 end
 
 describe "TileBag-Draw_Tiles" do
-  before do
-    @my_tiles = Scrabble::Tilebag.new
-    @display_tiles = Scrabble::Tilebag.new
-  end
-
   it "should return (num) number of random tiles and remove from tilebag" do
+    @my_tiles = Scrabble::Tilebag.new
     our_tiles = @my_tiles.draw_tiles(3)
     our_tiles.length.must_equal(3)
-    Scrabble::Tilebag::TILES.values.inject(:+).must_equal(94)
+    @my_tiles.default_tiles.values.inject(:+).must_equal(94)
   end
+end
 
-  it "should return the number of tiles remaining in the bag" do 
+describe "TilesRemaining" do
+  it "should return the number of tiles remaining in the bag" do
+  @display_tiles = Scrabble::Tilebag.new
   @display_tiles.draw_tiles(6)
   @display_tiles.tiles_remaining.must_equal(91)
   end
