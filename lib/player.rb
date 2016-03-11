59eaba68a696eccd733be520d52d3f6472b65307
Rowan Cota <cota.rg@gmail.com>
2016-03-11 13:51:04 -0800

    edited player to condense some more code after checking the coverage report and finding we had lines that never got hit.

--- lib/player.rb
+++ lib/player.rb
@@ -35,11 +35,7 @@ attr_accessor :total_score
 	def highest_scoring_word(array_of_words)
 		highest_score = ""
 		array_of_words.each do |word|
-		  if Scrabble::Scoring.score(word) > Scrabble::Scoring.score(highest_score)
-		    highest_score = word
-		  elsif Scrabble::Scoring.score(word) == Scrabble::Scoring.score(highest_score)
-		  	highest_score = word if word.length < highest_score.length
-		  end
+			highest_score = word  if Scrabble::Scoring.score(word) > Scrabble::Scoring.score(highest_score)
 	  end
 		return highest_score
 	end
@@ -47,11 +43,7 @@ attr_accessor :total_score
 	def highest_word_score
 		highest_score = ""
 		plays.each do |word|
-		  if Scrabble::Scoring.score(word) > Scrabble::Scoring.score(highest_score)
-		    highest_score = word
-		  elsif Scrabble::Scoring.score(word) == Scrabble::Scoring.score(highest_score)
-		  	highest_score = word if word.length < highest_score.length
-		  end
+			highest_score = word if Scrabble::Scoring.score(word) > Scrabble::Scoring.score(highest_score) 
 	  end
 		return Scrabble::Scoring.score(highest_score)
 	end
