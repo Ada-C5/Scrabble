13064740d06b53a50c11c262b7b41ea271399320
Leah <leahnpetersen@gmail.com>
2016-03-11 10:58:05 -0800

    Cleaned up player_spec, add two final tests with two player instances.

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -1,8 +1,8 @@
 require_relative './spec_helper'
 require_relative '../scrabble'
 
-#write a failing test
 describe Scrabble::Player do
+  # make sure there is a Player class
   it "Is there a class? Anything?" do
     Scrabble::Player.wont_be_nil
   end
@@ -60,17 +60,19 @@ describe Scrabble::Player do
     bob.highest_scoring_word.must_equal "ZZZZZZZ"
   end
 
-    # test to see highest_word_score returns correctly
+  # test to see highest_word_score returns correctly
   it "returns highest score" do
     bob = Scrabble::Player.new(name: "Bob", words: ["cat", "house", "zebra", "ZZZZZZZ"])
     bob.highest_word_score.must_equal 120
   end
 
+  # make sure player instances are created with 7 tiles to play with
   it "should start with 7 tiles " do
     bob = Scrabble::Player.new(name: "Bob", words: ["dog"])
     bob.tiles.length.must_equal(7)
   end
 
+  # make sure play and draw method work together to keep 7 tiles to play with
   it "should always have 7 tiles to play with after playing words " do
     bob = Scrabble::Player.new(name: "Bob", words: ["dog"])
     bob.tiles
@@ -78,4 +80,21 @@ describe Scrabble::Player do
     bob.tiles.length.must_equal(7)
   end
 
+  # holistic test with two player instances
+  it "check that Darren's score is 20 " do
+    bob = Scrabble::Player.new(name: "Bob", words: ["dog"])
+    darren = Scrabble::Player.new(name: "Darren", words: ["house"])
+    bob.play("mouse")
+    darren.play("box")
+    darren.total_score.must_equal(20)
+  end
+
+  # holistic test with two player instances PART 2!
+  it "check that Bob's score is 12 " do
+    bob = Scrabble::Player.new(name: "Bob", words: ["dog"])
+    darren = Scrabble::Player.new(name: "Darren", words: ["house"])
+    bob.play("mouse")
+    darren.play("box")
+    bob.total_score.must_equal(12)
+  end
 end
