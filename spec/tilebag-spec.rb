23c08dba29f4ea47f8ac96d72b79a4f3ede4d8c3
Adriana <dri19tcc@gmail.com>
2016-03-11 13:35:10 -0800

    Added a test to see if the TileBag class exists.  Made that test pass.  Added the method draw_tiles

--- spec/tilebag-spec.rb
+++ spec/tilebag-spec.rb
@@ -1,2 +1,12 @@
 require_relative './spec_helper'
 require_relative '../lib/tilebag'
+
+
+describe TileBag do
+  it "Does the TileBag Class exist test?" do
+    TileBag.wont_be_nil
+  end
+
+  
+
+end
