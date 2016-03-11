6801746c1d5c2d7a558a8c544595c3518036b805
Melissa Jimison <melissajimison@gmail.com>
2016-03-10 17:24:30 -0800

    Created method total_score

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,31 @@
 class Scrabble::Player
-  attr_accessor :name
+  attr_accessor :name, :array_words
+  #
 
   def initialize(name)
     @name = name
-    @array_of_words = []
+    @array_words = []
+  end
+
+  def plays
+    return @array_words
+    # => ["cat","pull","yes"]
   end
 
   def play(word) # takes word from player
-    @array_of_words << word
+    @array_words << word
+    Scrabble::Scoring.score(word)
+    # Return to a Numeric
   end
 
-  def plays # array
-    return @array_of_words
+  def total_score
+    array_of_scores = []
+
+    @array_words.each do |word|
+       array_of_scores << Scrabble::Scoring.score(word)
+    end
+    # => array_of_scores = [4, 6, 8,]
+    # array_of_scores.reduce(:+)
+    return array_of_scores
   end
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -6,19 +6,45 @@ describe Scrabble::Player do
     Scrabble::Player.must_be_instance_of Class
   end
 
+  PLAY_WORDS = ["pull", "yes"]
+  TEST_USER = Scrabble::Player.new("Jessica")
+
   describe "Scrabble::Player#plays" do
-    PLAY_WORDS = ["pull", "yes"]
 
     it "initializes the player with a name" do
-      Scrabble::Player.new("Test User").must_be_instance_of Scrabble::Player
+      Scrabble::Player.new("Jessica").must_be_instance_of Scrabble::Player
 
     end
 
-    test_user = Scrabble::Player.new("Test User")
     PLAY_WORDS.each do |word|
     it "play method returns arrays of words" do
-      test_user.play(word).must_be_kind_of Array
+      TEST_USER.plays.must_be_kind_of Array
     end
+    end
+  end
+
+  describe "Scrabble::Player#play(word)" do
+
+    PLAY_WORDS.each do |word|
+      it "should return the score of the word" do
+        TEST_USER.play(word).must_be_kind_of Numeric
+      end
+    end
+
+    it "should return the score of Melissa" do
+      TEST_USER.play("melissa").must_equal 59
+    end
+
+  end
+
+  describe "Scrabble::Player#total_score" do
+    melissa = Scrabble::Player.new("Meli")
+    it "should return the sum of scores played" do
+    test_words = ["melissa", "pull"]
+    test_words.each do |word|
+      # melissa.play(word)
+      melissa.total_score.must_equal 65
+      end
     end
   end
 end
