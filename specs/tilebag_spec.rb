5deeb78937a15996ad5ac7056fe97369b8131b9f
Leah <leahnpetersen@gmail.com>
2016-03-11 11:15:57 -0800

    Cleaned up tilebag_spec, added comments

--- specs/tilebag_spec.rb
+++ specs/tilebag_spec.rb
@@ -1,17 +1,19 @@
 require_relative './spec_helper'
 require_relative '../scrabble'
 
-#write a failing test
 describe Scrabble::TileBag do
+  # make sure there is a TileBag class
   it "Is there a class? Anything?" do
     Scrabble::TileBag.wont_be_nil
   end
 
+  # make sure a new instance creates a tilebag correctly
   it "should return 26 == number of letters in the alphabet" do
     game = Scrabble::TileBag.new
     game.tilebag.length.must_equal(26)
   end
 
+  # make sure draw_tiles method words correctly with passed parameter
   it "should return length of selected tiles" do
     game = Scrabble::TileBag.new
     game.draw_tiles(5).length.must_equal(5)
