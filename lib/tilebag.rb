4af33e31d67d8cd7db570255e20662ba4499282c
Rowan Cota <cota.rg@gmail.com>
2016-03-11 10:46:15 -0800

    set initializer to shuffle tiles on creation, wrote beginning of tile_draw using pop.

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -3,8 +3,14 @@ require_relative '../scrabble'
 class Scrabble::Tilebag
   attr_reader :tiles
 
-  def initialize
-    @tiles = ["A"]
-  end
+	def initialize
+		@tiles = Scrabble::DEFAULT_TILES.shuffle
+	end
 
+	def draw_tiles(num)
+		tiles_drawn = []
+		until tiles_drawn.length == num
+			tiles_drawn << tiles.pop		
+		end
+	end
 end
