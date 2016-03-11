e11f856e66bf032c2314f6506135682aaf732691
Rowan Cota <cota.rg@gmail.com>
2016-03-10 16:25:46 -0800

    playing with let and trying to figure out how to make it work

--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -6,26 +6,32 @@ describe Scrabble::Player do
   before do
     @jane = Scrabble::Player.new("Jane")
     @jane.play("cat")
-
+    
     @bob = Scrabble::Player.new("bob")
     @bob.total_score
-
+    
     @fred = Scrabble::Player.new("fred")
     @fred.play("cupcake")
     @fred.play("abalone")
-
   end
 
-  it "exists" do
-		Scrabble::Player.wont_be_nil
-	end
 
+
+  it "exists" do
+    Scrabble::Player.wont_be_nil
+  end
+
+  # this test works because a proc is an objct that holds the code and runs it at testing and returns the value
+  it "has a name" do
+    proc { Scrabble::Player.new() }.must_raise(ArgumentError)
+  end
+  
   it "has a readable name instance variable" do
     @jane.name.must_equal("Jane")
   end
 
   it "will return a value when total score is queried" do
-  	@jane.total_score.wont_be_nil
+    @jane.total_score.wont_be_nil
   end
 
   it "will return the score when a play is made" do
@@ -44,18 +50,15 @@ describe Scrabble::Player do
     @fred.won?.must_equal(true)
   end
 
-	it "returns highest scoring word in an array" do
-		@fred.highest_scoring_word(@fred.plays).must_equal("cupcake")
-	end
+  it "returns highest scoring word in an array" do
+    @fred.highest_scoring_word(@fred.plays).must_equal("cupcake")
+  end
 
   it "returns the score for highest scoring word" do 
     @fred.highest_word_score.must_equal(67)
   end
 
-
-
-  # it "will return false if the player has won" do
-  #   @jane.total_score
-  # end
-
+  it "will return false if a winner tries to make a play" do
+    @fred.play("cat").must_equal(false)
+  end
 end
